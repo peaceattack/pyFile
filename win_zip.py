2279eaa1413b4d722a00850b937a0de6a74213b8
peaceattack <13671018162@139.com>
2017-03-31 09:43:19 +0800

    win_zip.py
    windows版本

---
+++ win_zip.py
@@ -0,0 +1,10 @@
+
+ +# -*- coding:utf-8 -*-
+ +
+ +import zipfile
+ +MyZip = "C:\Users\Administrator\Desktop\MAC.zip"
+ +MyZipOBJ = zipfile.ZipFile(MyZip)
+ +MyZipOBJ.namelist()
+ +
+ +for i in MyZipOBJ.namelist():
+ +    print 
